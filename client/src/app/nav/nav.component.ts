248da0b0f351fcac4b5403eb3dad86cfbc002666
Alexey Khromets <alexchromets@gmail.com>
2022-01-05 17:53:11 +0200

    added login logic

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService,) {
+  }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model)
+      .subscribe(response => {
+        console.log(response);
+        this.loggedIn = true;
+      }, error => {
+        console.error(error)
+      });
   }
 
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => {
+      console.error(error);
+    });
+  }
 }
---
+++ client/src/app/services/account.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { User } from '../models/User';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
